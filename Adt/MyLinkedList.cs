055a65274d0d2e5ff0f01f680b1bd79c77dad237
Mia Pars Markussen <miax7855@edu.eal.dk>
2018-12-05 10:46:31 +0100

    Add project files.

---
+++ Adt/MyLinkedList.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adt
+{
+    public class MyLinkedList
+    {
+        //peger på den første instans af node
+        //antal elementer i listen
+        private int count;
+
+        public int Count
+        {
+            get { return count; }
+            set { count = value; }
+        }
+        //klassen Node intager et object som constructor
+        private Node Head;
+        private class Node
+        {
+            public Node(Object obj)
+            {
+                Data = obj;
+            }
+
+            private Object data;
+            public Object Data
+            {
+                get { return data; }
+                set { data = value; }
+            }
+
+            private Node next;
+            public Node Next
+            {
+                get { return next; }
+                set { next = value; }
+            }
+        }
+
+        public void Insert(Object obj)
+        {
+            Insert(obj, 0);
+            
+        }
+        public void Insert(Object obj, int index)
+        {
+            Node n = new Node(obj);
+            if (index == 0)
+            {
+                n.Next = Head;
+                Head = n;
+            }
+            else
+            {
+                Node temp = Head;
+                for (int i = 0; i < index - 1; i++)
+                {
+                    temp = temp.Next;
+                }
+                n.Next = temp.Next;
+                temp.Next = n;
+                Head = temp;
+
+                //Node temp = new Node(obj);
+                //temp = Head;
+                //Head.Next = temp;
+            }
+            
+
+            
+
+            n.Next = temp.Next;
+            temp.Next = n;
+        }
+        public void Delete()
+        {
+
+        }
+        public void Delete(int index)
+        {
+
+        }
+        public Object ItemAt(int index)
+        {
+
+        }
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
---
+++ Ex32-linkedlist/ClubMember.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex32_linkedlist
+{
+    public class ClubMember
+    {
+        private int nr;
+        private string fnavn;
+        private string lnavn;
+        private int alder;
+        public int Nr
+        {
+            get { return nr; }
+            set { nr = value; }
+        }
+        public string FNavn
+        {
+            get { return fnavn; }
+            set { fnavn = value; }
+
+        }
+        public string LNavn
+        {
+            get { return lnavn; }
+            set { lnavn = value; }
+
+        }
+        public int Alder
+        {
+            get { return alder; }
+            set { alder = value; }
+        }
+
+        public ClubMember(int nr, string Fname, string Lname, int alder)
+        {
+            Nr = nr;
+            FNavn = Fname;
+            LNavn = Lname;
+            Alder = alder;
+        }
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+    }
+}
